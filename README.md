4c836b44851cd521ae841fbb02e254402d7a022c
hb-p <bostrom.henrik@outlook.com>
2020-02-22 18:12:38 +0100

    feat: github api url stratified sampling.

--- README.md
+++ README.md
@@ -3,10 +3,72 @@
 ### Requirements
 
 * Java 1.8+
-* A data dump or remote connection to a GHTorrent DB
+* A data dump or remote connection to a GHTorrent PSQL DB
 
 ### Get Started
 
+**Execute the following query in the GHTorrent PSQL DB**
+
+```sql
+CREATE table project_samples
+AS
+SELECT url, language, COUNT(*) as nbr_of_stars
+FROM watchers
+INNER JOIN (
+    SELECT id, language, url 
+    FROM projects
+    WHERE language='C++' OR language='Java' -- Add more languages if needed
+) as projects
+ON projects.id = watchers.repo_id 
+GROUP BY url, language, repo_id;
 ```
-java -jar tool.jar --lang java --nbr-of-ranges 5 --tot-nbr-of-samples 1000 
+
+The query is stuck? Probably not, it's just large tables and a huge join.
+
+Why is this needed? This "staging" table improves the performance significantly.
+
+**Configure** `psql.properties`
+```
+url=jdbc:postgresql://localhost/ghtorrent_restore
+user=myusername
+password=mypassword
+```
+
+**Build**
+```
+mvn build
+```
+
+**Run**
+```
+java -jar tool.jar --lang java --nbr-of-ranges 5 --tot-nbr-of-samples 1000 --seed 1234 
+```
+
+### Arguments
+
+```
+--lang <String>, the language that program filters all repositories on.
+--nbr-of-ranges <Int>, the number of star ranges that you want to sample from.
+--tot-nbr-of-samples <Int>, the total number of samples.
+--seed <long>, use if you want to replicate the results.
+```
+
+### Example Output
+
+```
+Running with arguments: 
+* Language: C++
+* Number of samples: 321
+* Number of ranges: 5
+* Number of samples per range: 64
+* Seed: 1234
+
+[INFO] Looking for samples in range: 0-65436 (MAX: 327180) Progress: 0%
+[INFO] Looking for samples in range: 65436-130872 (MAX: 327180) Progress: 25%
+[INFO] Looking for samples in range: 130872-196308 (MAX: 327180) Progress: 50%
+[INFO] Looking for samples in range: 196308-261744 (MAX: 327180) Progress: 75%
+[INFO] Looking for samples in range: 261744-327180 (MAX: 327180) Progress: 100%
+[INFO] Writing samples to file samples.txt.
+
+Sample statistics, Max number of stars: 935 Min number of stars: 0
 ```
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,173 @@
-import java.sql.DriverManager;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static class CLIArguments {
+        String language;
+        int numberOfSamples;
+        int numberOfRanges;
+        long seed;
 
+        static int numberOfSamplesPerRange() {
+            return CLIArguments.numberOfSamples / CLIArguments.numberOfRanges;
+        }
+    }
+
+    static class Sample {
+        String url;
+        int numberOfStars;
+
+        Sample(String url, int numberOfStars) {
+            this.url = url;
+            this.numberOfStars = numberOfStars;
+        }
+    }
+
+    static CLIArguments CLIArguments = new CLIArguments();
+
+    public static void main(String[] args) {
+        // Simple argument parsing (I know that third party tools exist for argument parsing but I don't want to use them.)
+        for (int i = 0; i < args.length; i += 2) {
+            switch (args[i]) {
+                case "--lang":
+                    CLIArguments.language = args[i+1];
+                    break;
+                case "--nbr-of-ranges":
+                    CLIArguments.numberOfRanges = Integer.parseInt(args[i+1]);
+                    break;
+                case "--tot-nbr-of-samples":
+                    CLIArguments.numberOfSamples = Integer.parseInt(args[i+1]);
+                    break;
+                case "--seed":
+                    CLIArguments.seed = Long.parseLong(args[i+1]);
+                    break;
+                default:
+                    System.err.println("Invalid argument " + args[i]);
+                    return;
+            }
+        }
+
+        System.out.println("Sampling with arguments: ");
+        System.out.println("* Language: " + CLIArguments.language);
+        System.out.println("* Number of samples: " + CLIArguments.numberOfSamples);
+        System.out.println("* Number of ranges: " + CLIArguments.numberOfRanges);
+        System.out.println("* Number of samples per range: " + CLIArguments.numberOfSamplesPerRange());
+        System.out.println("* Seed: " + CLIArguments.seed + "\n");
+
+        // Find properties
+        Properties properties;
+        try (InputStream input = ClassLoader.getSystemResourceAsStream("psql.properties")) {
+            properties = new Properties();
+            properties.load(input);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            return;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+
+        // Connect to DB
+        Connection connection;
+        try {
+             connection = DriverManager.getConnection(properties.getProperty("url"), properties);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+
+        // Sample repositories
+        try {
+            sampleRepositories(connection, CLIArguments);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void sampleRepositories(Connection connection, CLIArguments CLIArguments) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        // Find number of repositories
+        ResultSet resultSet = statement.executeQuery(
+            "SELECT COUNT(*) FROM project_samples WHERE language='"+ CLIArguments.language +"'"
+        );
+        int numberOfRepositories = 0;
+        while (resultSet.next())
+        {
+            numberOfRepositories = Integer.parseInt(resultSet.getString(1));
+        }
+        resultSet.close();
+        int numberOfRepositoriesPerRange = numberOfRepositories / CLIArguments.numberOfRanges;
+
+
+        List<Sample> samples = new ArrayList<>();
+        Random rng = new Random(CLIArguments.seed);
+        // We use one RNG for each sample range because we don't want the samples subarray
+        // randomly picked indices to be the same for each range.
+        List<Random> rangeRNGs = new ArrayList<>();
+        for (int i = 0; i < CLIArguments.numberOfRanges; i++)
+        {
+            rangeRNGs.add(new Random(rng.nextLong()));
+        }
+
+        for (int i = 0; i < CLIArguments.numberOfRanges; i++)
+        {
+            System.out.println("[INFO] Looking for samples in range: "
+                    // Ex output: 149089-298178
+                    + numberOfRepositoriesPerRange * i + "-" + numberOfRepositoriesPerRange * (i + 1) +
+                    // Ex output: (MAX: 745445)
+                    " (MAX: " + (CLIArguments.numberOfRanges * numberOfRepositoriesPerRange) + ")"
+                    // Ex output: Progress: 25%
+                    + " Progress: " + (int)((double)i / (CLIArguments.numberOfRanges - 1) * 100) + "%");
+            resultSet = statement.executeQuery(
+                "SELECT * " +
+                "FROM project_samples " +
+                "WHERE language = '"+ CLIArguments.language +"' " +
+                "ORDER BY nbr_of_stars DESC " +
+                "LIMIT " + numberOfRepositoriesPerRange + " " +
+                "OFFSET " + numberOfRepositoriesPerRange * i + ";"
+            );
+
+            // Shuffle range and pick samples.
+            List<Sample> rangeSamples = new ArrayList<>();
+            while(resultSet.next())
+            {
+                rangeSamples.add(new Sample(resultSet.getString(1), Integer.parseInt(resultSet.getString(3))));
+            }
+            // Pick random range samples and put into the samples list.
+            Collections.shuffle(rangeSamples, rangeRNGs.get(i));
+            samples.addAll(rangeSamples.subList(0, CLIArguments.numberOfSamplesPerRange()));
+            resultSet.close();
+        }
+
+        statement.close();
+
+        // Write samples to file samples.txt.
+        try (FileWriter writer = new FileWriter("samples.txt")) {
+            System.out.println("[INFO] Writing samples to file samples.txt.");
+            for (Sample sample : samples) {
+                writer.write(sample.url + "\n");
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        // Print sample statistics
+        if (!samples.isEmpty()) {
+            int max = 0;
+            int min = 0;
+            for (Sample sample : samples) {
+                if (sample.numberOfStars < min) min = sample.numberOfStars;
+                if (sample.numberOfStars > max) max = sample.numberOfStars;
+            }
+            System.out.println("\nSample statistics:)");
+            System.out.println("* Max number of stars: " + max);
+            System.out.println("* Min number of stars: " + min);
+        }
     }
 }
